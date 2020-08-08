c9b0a7c59cc2dab52908215ba469a355b99f993a
Christopher Lindblom <lindblom0@outlook.com>
2020-08-08 23:38:46 +0200

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+    methods: {
+        sayGodbye(){
+            this.putGreeting('goodbye')
+        },
+        ...mapMutations({
+            putGreeting: 'putGreeting'
+        })
+    },
+    computed: {
+        ...mapState({
+            message: store => store.message
+        })
+    }
+}
+</script>
+
+<template>
+    <div>
+        {{message}}
+        <button @click="sayGodbye">Good bye</button>
+        <nuxt-link to="/">Home</nuxt-link>
+    </div>
+</template>
